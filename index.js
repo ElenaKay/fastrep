28fb2c8390f086e664ad2c021fa94a9c831442ae
l <l@l.l>
2020-02-21 23:04:59 +1100

    v:0.0.0-prealpha

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function fastrep(str, num) {
+  if (num >= 0) {
+    var result = "";
+    var cur = str;
+    while (true) {
+      if (num & 1)
+        result += cur;
+      num >>>= 1;
+      if (num !== 0)
+        cur += cur;
+      else {
+        return result;
+      }
+    }
+    assert(false);
+  }
+  assert(false, 'invalid length value: {'+recon(num)+'}');
+}
+
+module.exports.fastrep = fastrep;
